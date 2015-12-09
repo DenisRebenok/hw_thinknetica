470f4623bcd3b24c68a5a0397908ecb814e4a040
DenisRebenok <denis.rebenok.pro@gmail.com>
2015-12-10 00:53:34 +0200

    wrote an area of triangle calculator

---
+++ hw_1/area_triangle.rb
@@ -0,0 +1,9 @@
+puts "Какая длина основания треугольника?"
+base = gets.chomp.to_i
+puts "Какая высота треугольника?"
+height = gets.chomp.to_i
+
+area =  (base * height) / 2
+
+puts "Площадь треугольника: #{area}"
+
